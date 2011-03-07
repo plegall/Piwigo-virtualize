8e9ae9cb25c4c2ef7e4423cdb8e6b2d874869276
ddtddt <ddtddt@piwigo.org>
2011-03-07 21:29:57 +0000

    [extensions] -virtualize - add sv_SE thanks to jonas
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@9569 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2011      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Denna plugin flyttar alla dina bilder från <em>"gallerier"</em> (läggas till av synkroniseringsprocessen) till <em>"ladda upp"</em> och kategorier markera som virtuellaT.';
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Kontrollera att du har en säkerhetskopia av din <em>"gallerier"</em> katalog och en dump av databasen.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'När kategorierna är virtuella, kan du flytta dem hur du vill.';
+$lang['Start to virtualize'] = 'Börja med att virtualisera';
+?>
