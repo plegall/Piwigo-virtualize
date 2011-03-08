a4bec23afd5bfc0c4f7f710bdac9a4b03cd37e9e
dodo <dodo@piwigo.org>
2011-03-08 12:57:46 +0000

    git-svn-id: http://piwigo.org/svn/extensions/virtualize@9574 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2011      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Tento doplnok presunie všetky Vaše fotografie z <em>"galérií"</em> (pridané zo synchronizačného procesu) do <em>"nahratých"</em> a označí kategórie ako virtuálne.';
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Uistite sa, že máte vytvorenú zálohu z Vašich <em>"galériových"</em> adresárov a Vašej databázy.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Keď sú kategórie virtuálne, môžete ich presunúť kam chcete.';
+$lang['Start to virtualize'] = 'Štart virtualizácie';
+?>
