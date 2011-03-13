058dc9153abf67ffddaa895107b7dc6547b7eb06
samli <samli@piwigo.org>
2011-03-13 19:49:05 +0000

    [extensions] [virtualize] add HU language
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@9652 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2011      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Ez a bővítmény valamennyi képet áthelyezi a <em>"galleries"</em> könyvtárból (beleértve a szinkronizációt) az  <em>"upload"</em> könyvtárba, és a kategóriákat virtuálisként jelöli meg.';
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Meg kell bizonyosodni arról, hogy a <em>"galleries"</em> könyvtárról van egy biztonsági másolat, és dump az adatbázisról.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Amennyiben a kategóriák virtuálisak, tetszőlegesen áthelyezhetők.';
+$lang['Start to virtualize'] = 'Virtualizáció indítása';
+?>
