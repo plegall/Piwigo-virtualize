a918ba4e04079695c2f8db78eee0308639e43a56
ddtddt <ddtddt@piwigo.org>
2011-03-13 07:42:07 +0000

    [extensions] - virtualize - add es_ES thanks to AM
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@9637 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2011      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Este plugin traslada todas las imágenes del directorio <em>"galleries"</em> (añadidas mediante la sincronización) a <em>"upload"</em> y marca las categorías como virtuales.';
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Asegúrese de disponer de una copia de seguridad del directorio <em>"galleries"</em>, así como de un volcado de la base de datos.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Una vez sus categorías son virtuales, puede moverlas del modo que desee.';
+$lang['Start to virtualize'] = 'Iniciar la virtualización';
+?>
