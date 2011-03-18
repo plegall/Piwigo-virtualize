53aaae3220374a8293b9c9f1247374e555c2409a
ddtddt <ddtddt@piwigo.org>
2011-03-18 11:42:07 +0000

    [extensions] - virtualize - add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@9753 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2011      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Šis spraudnis pārvieto visas jūsu fotogrāfijas no <em>"galleries"</em> (pievienotas, izmantojot sinhronizācijas procesu) uz <em>"upload"</em> un marķē kategorijas kā virtuālas.';
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Pārliecinieties, ka esat izveidojuši jūsu <em>"galleries"</em> direktorijas kopiju un iztīrījis savu datu bāzi.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Līdz ko kategorijas ir virtuālas, jūs varat pārvietot tās kā vien vēlaties.';
+$lang['Start to virtualize'] = ' Sākt virtualizāciju';
+?>
