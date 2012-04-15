24e3d13a107c70c55c0d8fc371ee4a8f721eeb7a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-15 17:29:56 +0000

    [Virtualize] Add cs_CZ, thanks to : coudy
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@14171 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Ujistěte se, že máte zálohu Vašeho <em>"galleries"</em> adresáře a výpis (zálohu) Vaší databáze.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Jakmile se kategorie stanou virtuálními, lze je přesouvat jak Vy chcete.';
+$lang['Start to virtualize'] = 'Spusť Virtualizaci';
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Tento plugin přesune všechny Vaše fotky z <em>"galleries"</em> (přidané synchronizačním procesem) do <em>"upload"</em> a označí kategorie jako virtuální.';
+?>
