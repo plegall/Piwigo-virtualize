6a3c1aa3e8aa51c25914adaa75e7bd6f3945bb05
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-22 16:05:42 +0000

    [virtualize] Add nl_NL, thanks to : ruud
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@14265 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Zorg ervoor dat je een backup hebt van je <em>"galleries"</em> directory en een dump van je database.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Zodra de categorieën virtueel zijn, kun je ze verplaatsen waarheen je wilt.';
+$lang['Start to virtualize'] = 'Begin met virtualiseren';
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Deze plug-in verplaatst al je foto\'s van <em>"galleries"</em> (toegevoegd met het synchronisatieproces) naar <em>"upload"</em> en markeert de categorieën als virtueel.
+';
+?>
