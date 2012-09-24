873ce68fe729ede3061797c13f37644435cdde53
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-24 04:56:37 +0000

    [virtualize] Update th_TH, thanks to : pradit
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@18203 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/th_TH/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'ปลั๊กอินนี้ย้ายภาพถ่ายของคุณทั้งหมดจาก <em>"แกลเลอรี่"</em> (เสริมด้วยการ synchronize) เพื่อ <em>"อัปโหลด"</em>';
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'ให้แน่ใจว่าคุณมีการสำรองข้อมูลของ <em>"แกลเลอรี่ของคุณ"</em> ไดเรกทอรีและการถ่ายโอนข้อมูลจากฐานข้อมูลของคุณ';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'ประเภทอัลบั้มของคุณสามารถย้ายไปในแบบที่คุณต้องการ';
+$lang['Start to virtualize'] = 'เริ่มจำลอง';
+?>
