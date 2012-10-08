72840c982686b6e6d4716c65ddde954270126677
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-08 16:06:54 +0000

    [virtualize] Update fa_IR, thanks to : arashshiler
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@18555 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'اطمینان حاصل کنید که شما دارید یک پشتیبان از <em>"galleries"</em>فهرست و دیتابیس تان';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'هنگامی که دسته بندی ها مجاز هستند تو میتوانی جابجا کنی انها را هر جا که میخواهی';
+$lang['Start to virtualize'] = 'شروع به مجازی سازی';
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'این پلاگین جابجا میکند عکس هایتان را از <em>"galleries"</em>به <em>"upload"</em>';
+?>
