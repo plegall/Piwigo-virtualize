b8cb01ba14ee1f44e01a387d8d13843d92423d6e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-10 14:48:31 +0000

    [virtualize] Add bg_BG, thanks to : picsvet
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@21433 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Start to virtualize'] = 'Започнете виртуализиране';
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Уверете се, че имате резервно копие на<em>"галерии"</em>указател и уфт\'ейф на вашата база данни.';
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Приставката премества всички ваши снимки от<em>"галерии"</em>(добавени по време на сихронизационния процес) до <em>"прикачи"</em> и маркира категориите като виртуални.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'След като категориите са станали виртуални, можете да ги премествате по начина, по който искате.';
+?>
