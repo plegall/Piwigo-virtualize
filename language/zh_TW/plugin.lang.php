e9d6b3c97949ddd3a9e07168be5359d7c6a1c87e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-15 05:50:20 +0000

    [virtualize] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@21561 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = '這個插件將所有的照片從<em>"畫廊"</em>的同步過程（增加）來<em>"上載"</EM>和標記類別為虛。';
+$lang['Start to virtualize'] = '開始進行虛擬化';
+$lang['Once categories are virtual, you can move them the way you want.'] = '一旦類別是虛擬的，你可以移動你想要的方式。';
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = '請確保您有備份您的<em>"畫廊"</ em>的目錄和轉儲您的資料庫的備份。';
+?>
