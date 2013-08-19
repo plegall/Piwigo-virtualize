372cbe77960d126d24010d989c384ea70f97e4dc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-19 20:57:30 +0000

    [virtualize] Add eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@24190 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Certigu ke vi havas savkopion de via dosierujo <em>"galerioj"</em> kaj nekropsion de via datumbazo.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Post ke la kategorioj iĝas virtualaj, eblas ĉiel ĉien movi ilin.';
+$lang['Start to virtualize'] = 'Komenci la virtualigadon';
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Tiu kromaĵo movas ĉiujn fotojn viajn de <em>"galieroj"</em> (aldonita per la sinkroniga procezo) al <em>"elŝuti"</em> kaj markas la kategoriojn kiel virtualaj.';
+?>
