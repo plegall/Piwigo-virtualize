fa44bc7a7d4aea73d55489e5abfa2ec016e28999
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-12 18:59:00 +0000

    [virtualize] Add ta_IN, thanks to : saitjaleel
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@24425 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ta_IN/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'உங்கள் <em> "காட்சியகங்கள்" ஒரு காப்பு வைத்து கொள்ளுங்கள் </ em> அடைவு உங்கள் தரவுத்தள ஒரு திணிப்பு.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'பிரிவுகள் மெய்நிகர் பின், அவற்றை நீங்கள் எந்த வழியில் செல்ல முடியும்.';
+$lang['Start to virtualize'] = 'கற்பனையாக்கத்தை தொடங்க';
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'இந்த சொருகி <em> "காட்சியகங்கள்" அனைத்து உங்கள் புகைப்படங்கள் நகர்கிறது </ span> (ஒருங்கிணைப்பு பணியை சேர்க்க)
+  என்று <em> "பதிவேற்ற" </em> மற்றும் மெய்நிகர் என குறி பிரிவுகள்.';
+?>
