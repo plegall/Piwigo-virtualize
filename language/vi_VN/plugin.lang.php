373d9efa99a766fa528998e3e3e128e3b7808b60
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-02 19:12:44 +0000

    [virtualize] Add vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@24728 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Hãy đảm bảo bạn đã sao lưu thư mục <em>"galleries"</em> và cơ sở dữ liệu';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Một khi phân loại là ảo, bạn có thể di chuyển đi bất kỳ đâu';
+$lang['Start to virtualize'] = 'Bắt đầu ảo hóa';
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Plugin này sẽ chuyển tất cả ảnh từ <em>"galleries"</em> (được thêm vào bởi quá trình đồng bộ hóa) đến <em>"upload"</em> và đánh dấu phân loại là ảo.';
+?>
