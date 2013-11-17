18591e0404618992d214588859b04c747da5f6f2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-17 19:50:29 +0000

    [virtualize] Update br_FR, thanks to : yves.kerleguer
    git-svn-id: http://piwigo.org/svn/extensions/virtualize@25521 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/br_FR/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Al lugant-mañ a zilec\'h an holl luc\'hskeudennoù eus <em>"galleries"</em> (lakaet en ur sinkronelaat) da <em>"upload"</em>, hag a verk ar rummadoù evel galloudel.';
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Gwiriit m\'ho peus ur saveteadenn eus ho kavlec\'h <em>"galleries"</em> hag unan eus ho diaz roadennoù.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Ur wech galloudel ar rummadoù, posubl eo deoc\'h dilec\'hiañ anezho evel m\'ho peus c\'hoant.';
+$lang['Start to virtualize'] = 'Kergiñ gant ar galloudeladur';
+?>
