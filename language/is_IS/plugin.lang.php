c2f60ea32c03082be801396902a268ec80d41c2e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-16 06:22:30 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Gakktu úr skugga um að þú hafir öryggisafrit af <em>"myndasafna"</em> möppunni þinni og afrit af gagnagrunninum þínum.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Þegar flokkar eru orðnir sýndarflokkar þá geturðu fært þá hvernig sem þú vilt.';
+$lang['Start to virtualize'] = 'Byrjaðu sýndarvæðingu';
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Þessi viðbót færir allar myndirnar þínar úr <em>"myndasöfnum"</em> (bætt við með samstillingarferlinu) yfir í <em>"hlaða upp"</em> og  flokkar merktir sem sýndarflokkar.';
