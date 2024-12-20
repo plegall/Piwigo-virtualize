d1e9c4a766fd442879dabc514a982b738e1df363
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:09:50 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'ודא שיש לך גיבוי של התיקייה של ה<em>"galleries"</em> והעתק של מסד הנתונים שלך.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'לאחר שהקטגוריות הן וירטואליות, תוכל להעביר אותן בכל דרך שתרצה.';
+$lang['Start to virtualize'] = 'התחלת הוירטואליזציה';
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'התוסף מעביר את כל התמונות שלך מ<em>"galleries"</em> (נוספו בתהליך הסנכרון) ל<em>"upload"</em> ומסמן קטגוריות כוירטואליות.';
