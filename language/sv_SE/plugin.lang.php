4d1d42c0fb526d81ce04ec1a45af7dab18bbd467
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-18 07:07:29 +0200

    Update sv_SE, thanks to PBobbenB

--- language/sv_SE/plugin.lang.php
+++ language/sv_SE/plugin.lang.php
@@ -19,8 +19,12 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 
-$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Denna plugin flyttar alla dina bilder från <em>"gallerier"</em> (läggas till av synkroniseringsprocessen) till <em>"ladda upp"</em> och kategorier markera som virtuellaT.';
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Denna plugin flyttar alla dina foton från <em>"gallerier"</em> (läggs till av synkroniseringsprocessen) till <em>"ladda upp"</em> och markerar kategorier som virtuella.';
 $lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Kontrollera att du har en säkerhetskopia av din <em>"gallerier"</em> katalog och en dump av databasen.';
 $lang['Once categories are virtual, you can move them the way you want.'] = 'När kategorierna är virtuella, kan du flytta dem hur du vill.';
 $lang['Start to virtualize'] = 'Börja med att virtualisera';
-?>
+$lang['%d photos have been virtualized'] = '%d foton har virtualiserats.';
+$lang['%d photos to virtualize have no checksum yet...'] = '%d foton att virtualisera har ingen kontrollsumma ännu...';
+$lang['Compute them first in the batch manager'] = 'Beräkna dem först i batchhanteraren';
+$lang['nothing to virtualize, all is already virtual, you\'re good to go :-)'] = 'inget att virtualisera, alla är virtuella redan, det är bara att köra på :-)';
+$lang['you have %d photos to virtualize'] = 'du har %d foton att virtualisera';
