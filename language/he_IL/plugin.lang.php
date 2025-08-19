1408e365dbe7ac03cd20d14f25b2fced507b6b98
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-19 05:50:00 +0200

    Update he_IL, thanks to epifeny

--- language/he_IL/plugin.lang.php
+++ language/he_IL/plugin.lang.php
@@ -9,3 +9,8 @@ $lang['Make sure you have a backup of your <em>"galleries"</em> directory and a 
 $lang['Once categories are virtual, you can move them the way you want.'] = 'לאחר שהקטגוריות הן וירטואליות, תוכל להעביר אותן בכל דרך שתרצה.';
 $lang['Start to virtualize'] = 'התחלת הוירטואליזציה';
 $lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'התוסף מעביר את כל התמונות שלך מ<em>"galleries"</em> (נוספו בתהליך הסנכרון) ל<em>"upload"</em> ומסמן קטגוריות כוירטואליות.';
+$lang['%d photos have been virtualized'] = '%d תמונות הפכו לווירטואליות';
+$lang['%d photos to virtualize have no checksum yet...'] = 'ל-%d תמונות שעומדות להיות מווירטואלות אין עדיין סיכום ביקורת (checksum)...';
+$lang['Compute them first in the batch manager'] = 'חשב אותם תחילה במנהל האצווה';
+$lang['nothing to virtualize, all is already virtual, you\'re good to go :-)'] = 'אין את מה להפוך לויראוטלי, הכל כבר וירטואלי, אתם מסודרים :-)';
+$lang['you have %d photos to virtualize'] = 'יש לך %d תמונות להפוך לוירטואלים';
