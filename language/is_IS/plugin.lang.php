cac76dd9dd4111aa5236c753eeece5455f47b1f5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-19 05:50:14 +0200

    Update is_IS, thanks to gudjonsi

--- language/is_IS/plugin.lang.php
+++ language/is_IS/plugin.lang.php
@@ -9,3 +9,8 @@ $lang['Make sure you have a backup of your <em>"galleries"</em> directory and a 
 $lang['Once categories are virtual, you can move them the way you want.'] = 'Þegar flokkar eru orðnir sýndarflokkar þá geturðu fært þá hvernig sem þú vilt.';
 $lang['Start to virtualize'] = 'Byrjaðu sýndarvæðingu';
 $lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Þessi viðbót færir allar myndirnar þínar úr <em>"myndasöfnum"</em> (bætt við með samstillingarferlinu) yfir í <em>"hlaða upp"</em> og  flokkar merktir sem sýndarflokkar.';
+$lang['%d photos have been virtualized'] = '%d myndir hafa verið sýndarvæddar';
+$lang['%d photos to virtualize have no checksum yet...'] = '%d myndir til að sýndarvæða hafa ekkert checksum ennþá...';
+$lang['Compute them first in the batch manager'] = 'Reiknaðu þau fyrst í lotustjóranum';
+$lang['nothing to virtualize, all is already virtual, you\'re good to go :-)'] = 'ekkert að sýndarvæða, allt er nú þegar sýndarvætt, þú ert tilbúinn :-)';
+$lang['you have %d photos to virtualize'] = 'þú átt %d myndir til að sýndarvæða';
