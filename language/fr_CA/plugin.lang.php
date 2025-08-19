586624da94cd987525f8fa2bdd37a5d7c9b33a18
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-19 05:50:09 +0200

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Assurez vous d\'avoir une sauvegarde de votre répertoire <em>"galleries"</em> ainsi qu\'une sauvegarde de votre base de données.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Une fois que vos albums sont virtuels, vous pouvez les déplacer comme cela vous chante.';
+$lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Ce plugin déplace toutes vos photos du répertoire <em>"galleries"</em> (ajoutées par synchronisation) vers <em>"upload"</em> et déclare les albums virtuels.';
