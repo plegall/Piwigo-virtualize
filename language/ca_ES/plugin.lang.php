8d934246525a426f927cbc564c8959d161ae1197
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-19 05:50:03 +0200

    Update ca_ES, thanks to Tirs

--- language/ca_ES/plugin.lang.php
+++ language/ca_ES/plugin.lang.php
@@ -20,8 +20,12 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Comprovar que disposi d\'una còpia de seguretat del directori <em>"galleries"</em> i un bolcat de la base de dades.';
-$lang['Once categories are virtual, you can move them the way you want.'] = 'Quan els àlbums són virtuals es poden moure de la manera desitjada ';
+$lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = 'Comproveu que disposeu d\'una còpia de seguretat del directori <em>"galleries"</em> i un bolcat de la base de dades.';
+$lang['Once categories are virtual, you can move them the way you want.'] = 'Quan els àlbums són virtuals es poden moure de la manera que desitgeu';
 $lang['Start to virtualize'] = 'Inicia virtualització';
 $lang['This plugin moves all your photos from <em>"galleries"</em> (added with the synchronization process) to <em>"upload"</em> and mark categories as virtual.'] = 'Aquest connector mou totes les fotos del directori <em>"galleries"</em> (afegides amb el procés de sincronització) cap al directori <em>"upload"</em> i estableix els àlbums com a "virtuals".';
-?>
+$lang['nothing to virtualize, all is already virtual, you\'re good to go :-)'] = 'No hi ha res per virtualitzar, tot és ja virtual, ja heu fet la feina :-)';
+$lang['you have %d photos to virtualize'] = 'Teniu %d fotos per virtualitzar';
+$lang['Compute them first in the batch manager'] = 'Calculeu-les abans en el gestor de processament per lots';
+$lang['%d photos have been virtualized'] = 'S\'han virtualitzat %d fotos';
+$lang['%d photos to virtualize have no checksum yet...'] = '%d fotos per virtualitzar encara no tenen sumes de comprovació';
