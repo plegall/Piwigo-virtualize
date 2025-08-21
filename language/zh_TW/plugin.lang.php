e63043eb654e6b3aa4468a454786b4bee187695e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-08-21 06:23:41 +0200

    Update zh_TW, thanks to closer

--- language/zh_TW/plugin.lang.php
+++ language/zh_TW/plugin.lang.php
@@ -24,4 +24,8 @@ $lang['This plugin moves all your photos from <em>"galleries"</em> (added with t
 $lang['Start to virtualize'] = '開始進行虛擬化';
 $lang['Once categories are virtual, you can move them the way you want.'] = '一旦類別是虛擬的，你可以移動你想要的方式。';
 $lang['Make sure you have a backup of your <em>"galleries"</em> directory and a dump of your database.'] = '請確保您有備份您的<em>"畫廊"</ em>的目錄和轉儲您的資料庫的備份。';
-?>
+$lang['%d photos have been virtualized'] = '已將 %d 張照片虛擬化';
+$lang['%d photos to virtualize have no checksum yet...'] = '%d 張要進行虛擬化的照片還沒有 checksum...';
+$lang['Compute them first in the batch manager'] = '請先在「批次任務管理員」中進行計算';
+$lang['nothing to virtualize, all is already virtual, you\'re good to go :-)'] = '沒有要虛擬化的照片了。所有的照片都已經是虛擬的了，一切都準備好了 :-)';
+$lang['you have %d photos to virtualize'] = '您有 %d 張要虛擬化的照片';
